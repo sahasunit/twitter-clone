ca91e35e4a5d41899b7144c2e53221307ac6e034
Sunit Saha <sunitsaha@Sunits-Mac.local>
2024-11-02 11:27:53 +1030

    Auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { signUp, login, logout } from '../controllers/auth.controller.js';
+import { signUp, login, logout, getMe } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signUp);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;
