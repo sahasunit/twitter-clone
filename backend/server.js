fda1afd5bde9e9c2c9585729c24a07f569cc01e2
Sunit Saha <sunitsaha@Sunits-Mac.local>
2024-11-06 18:01:10 +1030

    Added User routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
+//Packages
 import express from "express";
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth.routes.js";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
+//Routes
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+//Utility functions
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +26,8 @@ app.use(express.json()); //to parse req.body
 app.use(express.urlencoded({extended: true})); //to parse form data(urlencoded)
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes); //Middleware
+app.use("/api/user", userRoutes); 
 
 app.listen(PORT, () => {
     connectMongoDB();
