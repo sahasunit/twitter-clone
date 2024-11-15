b663ec33a69102a3d382e281d904cfe6bc17897a
Sunit Saha <sunitsaha@Sunits-Mac.local>
2024-11-15 16:02:10 +1030

    fetch posts for the homepage

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -30,7 +30,7 @@ const Post = ({ post }) => {
 			<div className='flex gap-2 items-start p-4 border-b border-gray-700'>
 				<div className='avatar'>
 					<Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full overflow-hidden'>
-						<img src={postOwner.profileImg || "/avatar-placeholder.png"} />
+						<img src={postOwner.profileImg || "/src/public/avatar-placeholder.png"} />
 					</Link>
 				</div>
 				<div className='flex flex-col flex-1'>
@@ -85,7 +85,7 @@ const Post = ({ post }) => {
 												<div className='avatar'>
 													<div className='w-8 rounded-full'>
 														<img
-															src={comment.user.profileImg || "/avatar-placeholder.png"}
+															src={comment.user.profileImg || "/src/public/avatar-placeholder.png"}
 														/>
 													</div>
 												</div>
