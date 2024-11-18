07e0205904aec7faf736bc84692b0fc64b04cf3b
Sunit Saha <sunitsaha@Sunits-Mac.local>
2024-11-18 14:05:36 +1030

    fixed loginPage path in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
-import LoginPage from './pages/auth/login/loginPage'
+import LoginPage from './pages/auth/login/LoginPage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
